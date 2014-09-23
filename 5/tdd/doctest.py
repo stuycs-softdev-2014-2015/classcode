4456371d1d7824609d65f634709dd678965dfe5c
Mike Zamansky <zamansky@gmail.com>
2014-09-23 11:54:21 -0400

    Z

---
+++ 5/tdd/doctest.py
@@ -0,0 +1,16 @@
+import doctest
+
+
+def add2(a,b):
+    """
+    returns the addition of a and b
+    >>> add2(3,0)
+    3
+
+    >>> add2(3,4)
+    7
+    """
+    return a+b
+
+if __name__=="__main__":
+    doctest.testmod()
