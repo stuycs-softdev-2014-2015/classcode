a49da4ef5535dda9eae3d5a04ddf37a5df8ba45d
Mike Zamansky <zamansky@gmail.com>
2014-11-14 11:34:41 -0500

    Z

--- 5/adv_python/decorators.py
+++ 5/adv_python/decorators.py
@@ -4,6 +4,7 @@ def memoize(func):
     t={}
     @wraps(func)
     def inner(*args):
+        #print args
         if args in t:
             return t[args]
         else:
@@ -27,4 +28,31 @@ def fib2(n,t={}):
         r = fib2(n-2,t)+fib2(n-1,t)
         t[n]=r
         return r
-    
+
+@memoize
+def lcslength(s1,s2,i,j):
+    if i>=len(s1) or j >=len(s2):
+        return 0
+    elif s1[i]==s2[j]:
+        return 1+ lcslength(s1,s2,i+1,j+1)
+    else:
+        return max(lcslength(s1,s2,i+1,j),lcslength(s1,s2,i,j+1))
+import random
+import string
+def rstring(n):
+    return "".join(random.choice(string.ascii_uppercase+string.digits) for x in range(n))
+
+def dec_with_param(decargument):
+    print "declaring the dec_with_param"+decargument
+    def decorate(f):
+        def inner(*args,**kwargs):
+            print "in inner w/ decargument: "+decargument
+            result = f(*args,**kwargs)
+            return result
+        return inner
+    return decorate
+
+@dec_with_param("message")
+def hello():
+    return "hello world"
+
