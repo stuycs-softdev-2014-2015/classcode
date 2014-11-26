8bed6fe9fa8b71d7fc6d3798056f3733f43f6ea4
Mike Zamansky <zamansky@gmail.com>
2014-11-26 13:15:16 -0500

    Z

--- 7/javascript/intro.js
+++ 7/javascript/intro.js
@@ -4,7 +4,37 @@ console.log("HELLO");
 var i="hello";
 var j=20;
 
-var f1 = function(x) {
+var f = function(x) {
 		var j=30;
 		return j+x;
+};
+
+var o = { 'name' : 'joe',
+					age : 15,
+					items : [10, 20, 30, 40],
+					morestuff : {a : 1, b : 'hello'},
+					func : function(x) {
+							return x+30;
+					}
+				};
+
+var fact = function(n){
+		var prod=1;
+		for ( ; n > 1 ; n--){
+				prod = prod * n;
+		}
+		return prod;
 }
+
+var addItem = function(text){
+		var list = document.getElementById("thelist");
+		var newitem = document.createElement("li");
+		newitem.innerHTML = text;
+		list.appendChild(newitem);
+};
+
+var removeItem = function(n) {
+		var listitems = document.getElementsByTagName('li');
+		listitems[n].remove();
+};
+
