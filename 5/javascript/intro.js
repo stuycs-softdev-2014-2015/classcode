05fde4dc3d6d0d3a0a47ffef36c058f9cb1ceba7
Mike Zamansky <zamansky@gmail.com>
2014-11-26 11:44:59 -0500

    Z

--- 5/javascript/intro.js
+++ 5/javascript/intro.js
@@ -14,7 +14,37 @@ function f(x) {
 }
  */
 
-var f = function() {
+var f = function(x) {
 		return x + 1;
+};
+
+
+var fact = function(n){
+		var p = 1;
+		for (; n>1;n--)
+				p=p*n;
+		return p;
+};
+
+var o = {'name':'joe',
+				 age:15,
+				 n:[1,2,3,4,5],
+				 f : function(x) {
+						 return x+10;
+				 }
+				};
+
+var removeItem = function(num){
+		var list = document.getElementsByTagName('li');
+		list[num].remove();
+
 }
+
+var addItem = function(text) {
+		var list = document.getElementsByTagName('ol')[0];
+		var newitem = document.createElement('li');
+		newitem.innerHTML=text;
+		list.appendChild(newitem);
+}
+
 
