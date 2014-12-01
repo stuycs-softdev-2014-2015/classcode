ddc7db105390a96e84e087cb304d96c78c53ae21
Mike Zamansky <zamansky@gmail.com>
2014-12-01 12:23:30 -0500

    events

--- 6/javascript/intro.js
+++ 6/javascript/intro.js
@@ -40,11 +40,51 @@ var removeItem = function(num){
 		var list = document.getElementsByTagName('li');
 		list[num].remove();
 
-}
+};
 
 var addItem = function(text) {
 		var list = document.getElementsByTagName('ol')[0];
 		var newitem = document.createElement('li');
 		newitem.innerHTML=text;
 		list.appendChild(newitem);
+};
+
+var red = function() {
+		var items = document.getElementsByTagName("li");
+		for (var i = 0; i < items.length; i++){
+				items[i].classList.toggle('red');
+		}
+};
+
+var stripe = function() {
+		var items = document.getElementsByTagName("li");
+		for (var i = 0; i < items.length; i++){
+				if (i%2==0){
+						items[i].classList.add('red');
+				} else {
+						items[i].classList.add('blue');
+				}
+		}
+};
+
+var buttonCallback = function(e){
+		console.log(e);
+		console.log(this);
+};
+var button = document.getElementById("b");
+button.addEventListener('click',buttonCallback);
+
+var redCallback = function(){
+		console.log(this);
+		this.classList.add('red');
 }
+
+var thelist = document.getElementById("thelist");
+var litems = thelist.children;
+for (var i = 0 ; i < litems.length ; i++ ){
+		litems[i].addEventListener('click',redCallback);
+		litems[i].addEventListener('mouseover',function(e){
+				this.classList.toggle('green');
+		});
+}
+
