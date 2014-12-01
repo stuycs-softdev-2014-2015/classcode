c460322a9ac597b00a5a1eee09cec68ae5b17f10
Mike Zamansky <zamansky@gmail.com>
2014-12-01 13:08:04 -0500

    Z

--- 7/javascript/intro.js
+++ 7/javascript/intro.js
@@ -24,7 +24,7 @@ var fact = function(n){
 				prod = prod * n;
 		}
 		return prod;
-}
+};
 
 var addItem = function(text){
 		var list = document.getElementById("thelist");
@@ -38,3 +38,47 @@ var removeItem = function(n) {
 		listitems[n].remove();
 };
 
+var red = function() {
+		var items = document.getElementsByTagName("li");
+		for(var i = 0; i < items.length; i++) {
+				items[i].classList.add('red');
+		}
+};
+
+var stripe = function() {
+		var items = document.getElementsByTagName("li");
+		for(var i = 0; i < items.length; i++) {
+				if (i%2==0){
+						items[i].classList.add('red');
+				} else {
+						items[i].classList.add('blue');
+				}
+		}
+};
+
+var buttonCallback = function(e) {
+		console.log(e);
+		console.log(this);
+}
+
+var b = document.getElementById('b');
+b.addEventListener('click',buttonCallback);
+
+var redCallback = function(e) {
+		console.log(this);
+		this.classList.add('red');
+}
+
+var thelist = document.getElementById("thelist");
+var litems = thelist.children;
+for(var i = 0; i < litems.length; i++) {
+		litems[i].addEventListener('click',redCallback);
+		litems[i].addEventListener('mouseover',function(e){
+				console.log("in"+this);
+				this.classList.toggle('green');
+		});
+		litems[i].addEventListener('mouseout',function(e){
+				console.log("out"+this);
+				this.classList.toggle('blue');
+		});
+}
