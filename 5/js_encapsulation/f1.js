be43a18a850dbcac31d557863d4a3d947d406967
Mike Zamansky <zamansky@gmail.com>
2014-12-04 11:47:59 -0500

    Z

---
+++ 5/js_encapsulation/f1.js
@@ -0,0 +1,47 @@
+
+var base = 10;
+
+var otherfunc = function() {
+		console.log('the other function f1');
+};
+
+var f1 = {
+		base : 30,
+		f : function() {
+				console.log(base);
+				this.base = this.base + 2;
+				console.log(f1.base);
+				console.log(this.base);
+		},
+		f2 : otherfunc
+		
+};
+
+
+var makeincrementer = function(){
+		var i = 0;
+		return function() {
+				i = i + 1;
+				return i;
+		};
+};
+
+var makeAdder = function(n){
+		return function(x) {
+				return n+x;
+		};
+};
+
+
+var makeCounter = function() {
+		var i = 0;
+		var get =  function() { return i;},
+		return { x : "some value",
+						 get : get,
+						 inc : function() {i=i+1;},
+						 dec : function() { i = i - 1;},
+						 set : function(x) {i = x;},
+						 fx : function() { console.log(this.x);} // this to get to the local
+ 					 };
+};
+
---
+++ 5/js_encapsulation/f2.js
@@ -0,0 +1,11 @@
+
+var otherfunc = function() {
+		console.log('the other function f2');
+};
+
+var f2 = {
+		base : 100,
+		f : function(x) {
+				return x * base;
+		}
+};
