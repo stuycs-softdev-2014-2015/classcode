0dbb3a311b90bbb8cf2efc92e4fee7182519cf76
Mike Zamansky <zamansky@gmail.com>
2014-12-10 13:16:08 -0500

    Z

---
+++ 7/quotes/app.py
@@ -0,0 +1,15 @@
+from flask import Flask,request,url_for,redirect,render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
+if __name__=="__main__":
+   app.debug=True
+   app.run(host="0.0.0.0",port=8000)
