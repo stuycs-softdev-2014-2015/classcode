53caa22c17937403bae23e3ed1c6fde1b3b01f00
Mike Zamansky <zamansky@gmail.com>
2014-12-11 13:19:51 -0500

    Z

--- 7/quotes/static/script.js
+++ 7/quotes/static/script.js
@@ -1,19 +1,52 @@
 
 var quoteFactory = function() {
 
-		var addQuote = function(d){
+		var makeQuoteElt = function(d){
 				var q = "<blockquote><p>"+d.quote+"</p></blockquote>";
+				return q;
+		};
+		
+		var addQuote = function(d){
+				var q = makeQuoteElt(d);
 				$("#aquote").append(q);
 		};
 
-		return { add : addQuote
+		var getAddQuote = function() {
+				$.getJSON("/quote",function(d){
+						addQuote(d);
+				});
+		};
+
+		var clearQuotes = function() {
+				$("#aquote").empty();
+		};
+
+		var changeRandomQuote = function() {
+				$.getJSON("/quote",function(d){
+						var q = makeQuoteElt(d);
+						var elt = $("#rquote");
+						elt.slideUp(2000, function() {
+								elt.empty();
+								elt.append(q);
+								elt.slideDown(2000);
+							});
+				});
+		};
+
+		var interval ; // public variabel to hold the timer
+		 
+		return { add : getAddQuote,
+						 clear : clearQuotes,
+						 rand : changeRandomQuote,
+						 interval : interval
 					 };
 };
 
-
-var quotes;
 $(document).ready(function(){
 		console.log("HELLO");
-		quotes = quoteFactory();
+		var quotes = quoteFactory();
+		$("#add").bind('click',quotes.add);
+		$("#clear").bind('click',quotes.clear);
+		quotes.interval = setInterval(quotes.rand,7000);
 });
 
