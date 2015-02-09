14387c1b19796789ac0cba7d8c493ea16e964daf
Mike Zamansky <zamansky@gmail.com>
2015-02-09 12:26:27 -0500

    Z

--- 6/canvas/click.js
+++ 6/canvas/click.js
@@ -1,11 +1,9 @@
-
- 
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 
 var clicked = function(e){
 		e.preventDefault();
-		//ctx.beginPath();
+		ctx.beginPath();
 		ctx.arc(e.offsetX,e.offsetY,
 						15,0,2*Math.PI);
 		ctx.fillStyle="#ff0000";
@@ -16,7 +14,7 @@ var clicked = function(e){
 var clear = function(e){
 		ctx.fillStyle = "#ffffff";
 		ctx.fillRect(0,0,600,600);
-		
+		ctx.beginPath();
 };
 c.addEventListener("click",clicked);
 var b = document.getElementById("b");
