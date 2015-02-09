da6fdc0e6e2fbcc870cfed51af3a84b8321a99bf
Mike Zamansky <zamansky@gmail.com>
2015-02-09 11:37:12 -0500

    z

--- 5/canvas/canvas.js
+++ 5/canvas/canvas.js
@@ -1,27 +1,28 @@
+
+ 
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 
-ctx.fillStyle="#00ff00";
+ctx.fillStyle="#0000ff";
 ctx.fillRect(20,20,70,100);
 
-ctx.strokeStyle="#0000ff";
+ctx.strokeStyle="#ff0000";
 ctx.strokeRect(20,20,70,100);
 
 ctx.beginPath();
-ctx.strokeStyle="#00ff00";
-ctx.moveTo(300,300);
-ctx.lineTo(350,350);
-ctx.lineTo(300,0);
+ctx.strokeStyle="#0000ff";
+ctx.moveTo(200,200);
+ctx.lineTo(300,300);
+ctx.lineTo(200,350);
 ctx.closePath();
-ctx.fill();
 ctx.stroke();
-
+ctx.fill();
 ctx.beginPath();
-ctx.arc(100,100,50,Math.PI/4,Math.PI);
-ctx.fillStyle="#ff0000";
+ctx.arc(400,100,50,Math.PI,2*Math.PI);
+ctx.stroke();
 ctx.fill();
 ctx.fillStyle="#ffff00";
 
 ctx.font = "30px arial";
-ctx.fillText("Hello World",100,100);
+ctx.fillText("Hello World",200,250);
 ctx.stroke();
--- 5/canvas/click.js
+++ 5/canvas/click.js
@@ -1,14 +1,25 @@
+
+ 
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 
-var clicked = function(e) {
+var clicked = function(e){
 		e.preventDefault();
-		ctx.beginPath();
-		ctx.arc(e.offsetX,e.offsetY,15,0,2*Math.PI);
+		//ctx.beginPath();
+		ctx.arc(e.offsetX,e.offsetY,
+						15,0,2*Math.PI);
 		ctx.fillStyle="#ff0000";
-		ctx.fill();
 		ctx.stroke();
+		ctx.fill();
 };
 
-
+var clear = function(e){
+		ctx.fillStyle = "#ffffff";
+		ctx.fillRect(0,0,600,600);
+		ctx.beginPath();
+		
+};
 c.addEventListener("click",clicked);
+var b = document.getElementById("b");
+b.addEventListener("click",clear);
+
