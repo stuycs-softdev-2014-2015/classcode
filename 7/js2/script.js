6df8c579046622fde14a0671b5946549148c96da
Mike Zamansky <zamansky@gmail.com>
2015-02-24 13:19:31 -0500

    Z

--- 7/js2/script.js
+++ 7/js2/script.js
@@ -1,21 +1,53 @@
+var rawschooldata = rawscores.data;
 
-console.log(rawscores);
-
-rawschooldata = rawscores.data;
-console.log(rawschooldata);
-
-school_scores = [];
-for (var i=0; i < rawschooldata.length;i++){
-		var tmp = {
-				code:rawschooldata[i][8],
-				name:rawschooldata[i][9],
-				num:rawschooldata[i][10],
-				eng:rawschooldata[i][11],
-				math:rawschooldata[i][12],
-				writing:rawschooldata[i][13]
+var map = function(l,f){
+		var result = [];
+		for (i = 0; i < l.length; i++){
+				result.push(f(l[i]));
 		};
-		school_scores.push(tmp);
+		return result;
 };
 
 
-console.log(school_scores);
+var filter = function(l,f){
+		var result = [];
+		for (i = 0; i < l.length; i++){
+				if (f(l[i])){
+						result.push(l[i]);
+				}
+		};
+		return result;
+};
+
+var reduce = function(l,f,init){
+		var result = init;
+		for (i = 0; i < l.length; i++){
+				result = f(l[i],result);
+		};
+		return result;
+		
+};
+scores = map(rawschooldata,
+						 function(item){
+								 return {
+										 code:item[8],
+										 name:item[9],
+										 num:item[10],
+										 eng:item[11],
+										 math:item[12],
+										 writing:item[13]
+								 };
+						 });
+
+var rawmath = map(scores,function(item){return item.math;});
+rawmath = map(rawmath,function(x){return parseInt(x);});
+var mathscores = filter(rawmath,function(x){return !isNaN(x);});
+var sum = reduce(mathscores,function(a,b){return a+b;},0);
+var avg = Math.floor(sum/mathscores.length);
+console.log(avg);
+
+var goodmath = filter(scores,function(item){
+		return parseInt(item.math)>avg;
+});
+
+console.log(goodmath);
