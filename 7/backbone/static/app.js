9c592f35c0603858da945dd0f0286062766d20bc
Mike Zamansky <zamansky@gmail.com>
2015-03-09 13:08:53 -0400

    Z

--- 7/backbone/static/app.js
+++ 7/backbone/static/app.js
@@ -21,4 +21,21 @@ var Place = Backbone.Model.extend({
 		}
 });
 
+var PlaceView = Backbone.View.extend({
+		el : "#place",
+		initialize : function() {
+				this.render();
+		},
+		render:function() {
+				var t = _.template("<tr><td><%= name %></td><td><%= rating %></td></tr>");
+				
+				var e = t(this.model.toJSON());
+				// this.$el.empty();
+				this.$el.append(e);
+				return this;
+		}
+});
+
+
 var p1 = new Place({name:"Terry's",rating:5});
+var v1 = new PlaceView({model:p1});
