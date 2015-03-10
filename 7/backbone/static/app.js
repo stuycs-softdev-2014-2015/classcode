a0dd19fc062acbb598bde08a4cb7b9bb498d0159
Mike Zamansky <zamansky@gmail.com>
2015-03-10 13:05:29 -0400

    Z

--- 7/backbone/static/app.js
+++ 7/backbone/static/app.js
@@ -23,14 +23,33 @@ var Place = Backbone.Model.extend({
 
 var PlaceView = Backbone.View.extend({
 		el : "#place",
+		template :  _.template( $("#place_template").html() ),
+		events : {
+				"click .del" : function(e){
+						this.remove();
+				},
+				"click .up" : function(e){
+						var r = this.model.get("rating");
+						r = parseInt(r);
+						r = r + 1;
+						this.model.set("rating",r);
+						this.render();
+				},
+				"click .down" : function(e){
+						var r = this.model.get("rating");
+						r = parseInt(r);
+						r = r - 1;
+						this.model.set("rating",r);
+						this.render();
+				}
+				
+		},
 		initialize : function() {
 				this.render();
 		},
 		render:function() {
-				var t = _.template("<tr><td><%= name %></td><td><%= rating %></td></tr>");
-				
-				var e = t(this.model.toJSON());
-				// this.$el.empty();
+				var e = this.template(this.model.toJSON());
+				this.$el.empty();
 				this.$el.append(e);
 				return this;
 		}
