bbf675ea452b1d90a8c9f67004ca9cc46bf136a4
Mike Zamansky <zamansky@gmail.com>
2015-03-25 13:19:46 -0400

    Z

--- 7/marionette/static/app.js
+++ 7/marionette/static/app.js
@@ -8,10 +8,11 @@ App.addRegions({
 		thirdRegion: "#third-region",
 		fourthRegion:"#fourth-region"
 });
+
 App.on("start",function(){
 		console.log("STARTING");
 		var staticView = new App.StaticView();
-		App.firstRegion.show(staticView);
+		App.fourthRegion.show(staticView);
 
 		var placeView = new App.PlaceView({model:p1});
 		App.secondRegion.show(placeView);
@@ -19,8 +20,11 @@ App.on("start",function(){
 		var placesView = new App.PlacesView({collection:c});
 		App.thirdRegion.show(placesView);
 
-		var compView = new App.CompView({collection:c});
-		App.fourthRegion.show(compView);
+		var compView = new App.CompView({collection:c, model : person});
+		App.firstRegion.show(compView);
+
+		Backbone.history.start();
+
 });
 
 App.StaticView = Marionette.ItemView.extend({
@@ -47,6 +51,10 @@ App.CompView = Marionette.CompositeView.extend({
 		template : "#composite-template",
 		childView : App.PlaceView,
 		childViewContainer : "tbody",
+		modelEvents: {
+				"change":function(){
+						this.render();
+				}},
 		events : {
 				"click #add" : function(){
 						var n = $("#newname").val();
@@ -65,7 +73,39 @@ var Places = Backbone.Collection.extend({
 		comparator:"name"
 });
 
+var Person = Backbone.Model.extend();
+var person = new Person({last : 'Kat',
+												 first :'Fred',
+												 stars : 12
+												});
+
 var p1 = new Place({name:"Terry's",rating:5});
 var p2 = new Place({name:"Ferry's",rating:8});
 var c = new Places([p1,p2]);
+
+var myController = Marionette.Controller.extend({
+		default : function() {
+				var compView = new App.CompView({collection:c, model : person});
+				App.firstRegion.show(compView);
+		},
+		oneRoute : function() {
+				App.firstRegion.show(new App.PlaceView({model:p1}));
+				App.secondRegion.show(new App.PlaceView({model:p2}));
+		},
+		twoRoute : function() {
+				App.firstRegion.show(new App.PlaceView({model:p2}));
+				App.secondRegion.show(new App.PlaceView({model:p1}));
+		}
+});
+App.controller = new myController();
+
+App.router = new Marionette.AppRouter({
+		controller : App.controller,
+		appRoutes : {
+				"/" : "default",
+				one : "oneRoute",
+				two : "twoRoute"
+		}
+});
+
 App.start();
