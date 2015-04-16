99f8231c8ed3fce1a5456b8439f8fd068375d608
Mike Zamansky <zamansky@gmail.com>
2015-04-16 12:05:21 -0400

    Z

---
+++ 6/d3/svg1.js
@@ -0,0 +1,30 @@
+console.log("HELLO");
+
+var content = d3.select("#content");
+var svg = content.append("svg")
+				.attr("width","600")
+				.attr("height","600")
+				.classed("bordered",true)
+				.attr("id","svg");
+
+var c1 = svg.append("circle")
+				.attr({'cx':100,
+							 'cy':100,
+							 'r':20,
+							 'fill':'red'});
+
+var c2 = svg.append("circle")
+				.attr({'cx':300,
+							 'cy':300,
+							 'r':20,
+							 'fill':'green'
+							});
+
+var move = function(item,newx,newy){
+		item.transition()
+				.duration(4000)
+				.ease("bounce")
+				.attr("cx",newx)
+				.attr('cy',newy);
+};
+
